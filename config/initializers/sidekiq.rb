80d8d7de078af72a831d657547859a244506319c
gong023 <gon.gong.gone@gmail.com>
2014-07-07 22:37:06 +0900

    fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,21 +1,21 @@
 Sidekiq.configure_server do |config|
   env = ENV['RAILS_ENV'] ? ENV['RAILS_ENV'] : 'development'
-  config.redis = { url: ENV['DATABASE_URL'], namespace: "sidekiq_#{env}" }
+  config.redis = { url: ENV['REDISTOGO_URL'], namespace: "sidekiq_#{env}" }
 
-  database_url = ENV['DATABASE_URL']
+  database_url = ENV['REDISTOGO_URL']
   if database_url
-    ENV['DATABASE_URL'] = "#{database_url}?pool=25"
+    ENV['REDISTOGO_URL'] = "#{database_url}?pool=25"
     ActiveRecord::Base.establish_connection
   end
 end
 
 Sidekiq.configure_client do |config|
   env = ENV['RAILS_ENV'] ? ENV['RAILS_ENV'] : 'development'
-  config.redis = { url: ENV['DATABASE_URL'], namespace: "sidekiq_#{env}" }
+  config.redis = { url: ENV['REDISTOGO_URL'], namespace: "sidekiq_#{env}" }
 
-  database_url = ENV['DATABASE_URL']
+  database_url = ENV['REDISTOGO_URL']
   if database_url
-    ENV['DATABASE_URL'] = "#{database_url}?pool=25"
+    ENV['REDISTOGO_URL'] = "#{database_url}?pool=25"
     ActiveRecord::Base.establish_connection
   end
 end
